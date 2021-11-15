f903cabf18a804d323beab74123ff3672fc3a7f0
CSIWill <91165874+CSIWill@users.noreply.github.com>
2021-11-14 23:54:46 -0500

    final

--- js/my_blog.js
+++ js/my_blog.js
@@ -4,63 +4,61 @@ const content = document.querySelector('main');
 let myContent = mainCommentButton.parentNode;
 // console.log(myContent);
 
-// Add to Main Post
-function replyMain() {
+// allow user to enter information
+
+function replyMain(e) {
     let replies = document.createElement('section');
     let replyWindow = document.createElement('textarea');
     replyWindow.setAttribute('label','Enter Response');
     let postReply = document.createElement('button');
     postReply.innerHTML = "Post";
-    myContent = mainCommentButton.parentNode;
-      
+    let innerReply = document.createElement('div');
+    let replyMessage = document.createElement('p');
+    // replyMessage.innerHTML = replyWindow.value;
+    let replyButton = document.createElement('button');
+    replyButton.setAttribute('class','reply');
+    replyButton.innerHTML = "Reply";
+    if (e.target.className == 'input') {
+        myContent = mainCommentButton.parentNode;
+        console.log(myContent);
+        console.log("hello");
+    }
+    // else {
+    //     myContent = replyWindow.parentNode;
+    // }
+
     myContent.append(replies);
     console.log(myContent)
     replies.append(replyWindow);
     replies.append(postReply);
-
     postReply.addEventListener('click',reReply);
     postReply.setAttribute('class','post');
     console.log(postReply);
+    console.log(e.target.className);
+    
+    // save information and display
+
     function reReply() {
-        let innerReply = document.createElement('div');
-        let replyMessage = document.createElement('p');
+        // let innerReply = document.createElement('div');
+        // let replyMessage = document.createElement('p');
         replyMessage.innerHTML = replyWindow.value;
-        let replyButton = document.createElement('button');
-        replyButton.setAttribute('class','reply');
-        replyButton.innerHTML = "Reply";
+        // let replyButton = document.createElement('button');
+        // replyButton.setAttribute('class','reply');
+        // replyButton.innerHTML = "Reply";
         replies.append(innerReply);
         innerReply.append(replyMessage);
         innerReply.append(replyButton);
-        // let rreplies = document.createElement('section');
-        //     let rreplyWindow = document.createElement('textarea');
-        //     rreplyWindow.setAttribute('label','Enter Response');
-        //     let rpostReply = document.createElement('button');
+        
+        // get rid of entry box and reply button
+
         replyWindow.remove();
         postReply.remove();
-        // rreplyWindow.remove();
-        // rpostReply.remove();
+
         replyButton.addEventListener('click',replyMain);
-        // replyButton.addEventListener('click',reReplyMain);
-        function reReplyMain() {
-            // let rreplies = document.createElement('section');
-            // let rreplyWindow = document.createElement('textarea');
-            // rreplyWindow.setAttribute('label','Enter Response');
-            // let rpostReply = document.createElement('button');
-            rpostReply.innerHTML = "Post";
-            myContent = replyButton.parentNode;
-            myContent.append(rreplies);
-            console.log(myContent)
-            rreplies.append(rreplyWindow);
-            rreplies.append(rpostReply);
-            rpostReply.addEventListener('click',reReply);
-        }
+        myContent = replyButton.parentNode;
+
+        console.log(e.target.className);
+
     };
-    // mainCommentButton.addEventListener('click',replyMain);
-    
-    console.log(mainCommentButton);
 };
 mainCommentButton.addEventListener('click',replyMain);
-// commentButton = document.querySelectorAll('.reply');
-// commentButton[0].addEventListener('click',replyMain);
-
-// mainCommentButton.addEventListener('click',replyMain,mainCommentButton.parentNode);
