e7028579c62d563358a559baaf33e18c495fcd3f
Will Eng <weng@mavenitinc.com>
2021-12-01 14:01:34 -0500

    Test Question

--- index.js
+++ index.js
@@ -5,23 +5,6 @@ const ejs = require("ejs");
 // Create express app
 const app = express();
 
-// // Create a database connection configuration
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "root123",
-//   database: "testfullstack_students", // comment out if running example 1
-// });
-
-// // Establish connection with the DB
-// db.connect((err) => {
-//   if (err) {
-//     throw err;
-//   } else {
-//     console.log(`Successful connected to the DB....`);
-//   }
-// });
-
 // Initialize Body Parser Middleware to parse data sent by users in the request object
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to parse HTML form data
@@ -46,57 +29,16 @@ app.get("/my_fun_facts", (req, res) => {
 app.get("/my_trivia", (req, res) => {
   res.render("./html/my_trivia");
 });
-app.post("/question1", (req,res) => {
+
+app.post("/my_trivia", (req,res) => {
   let userAnswer = req.body.userAnswer1
-  if (userAnswer.toUpperCase() === "D") {
+  if (userAnswer == "D") {
     res.send(`Correct`);  
   }
   else {
     res.send(`Incorrect`);
   }
-  
 });
-
-// app.post("/insertstudents", (req, res) => {
-//   let data = { name: req.body.studentName, email: req.body.studentEmail };
-//   let sql = `INSERT INTO students SET ?`;
-//   let query = db.query(sql, data, (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//     res.send(`student entry was inserted to the db...`);
-//   });
-// });
-
-// app.post("/updatestudents", (req, res) => {
-//   let sql = `UPDATE students SET email = '${req.body.studentNewEmailUpdate}'  WHERE id = ${req.body.studentID}`;
-//   db.query(sql, (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//     res.send(`student entry was updated in the db...`);
-//   });
-// });
-
-// app.post("/deletestudents", (req, res) => {
-//   let sql = `DELETE FROM students WHERE email = '${req.body.studentEmail}'`;
-//   db.query(sql, (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//     res.send(`student entry was deleted in the db...`);
-//   });
-// });
-
-// app.get("/readstudents", (req, res) => {
-//   let sql = `SELECT * FROM students`;
-//   db.query(sql, (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//     res.render("readData", { data: result });
-//   });
-// });
 
 // Setup server ports
 const PORT = process.env.PORT || 3000;
