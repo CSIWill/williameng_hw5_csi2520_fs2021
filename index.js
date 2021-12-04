ede385aa7a02c7f16d22174890b837c4ba4d3da4
CSIWill <91165874+CSIWill@users.noreply.github.com>
2021-12-04 15:18:01 -0500

    1 question

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 // const mysql = require("mysql");
-const ejs = require("ejs");
+const ejs = require('ejs');
 
 // Create express app
 const app = express();
@@ -10,34 +10,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to parse HTML form data
 
 // Initialize ejs Middleware
-app.set("view engine", "ejs");
-app.use("/public", express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
 
 // routes
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/my_resume", (req, res) => {
-  res.render("./html/my_resume.ejs");
+app.get('/my_resume', (req, res) => {
+  res.render('./html/my_resume.ejs');
 });
 
-app.get("/my_fun_facts", (req, res) => {
-  res.render("./html/my_fun_facts");
+app.get('/my_fun_facts', (req, res) => {
+  res.render('./html/my_fun_facts');
 });
 
 app.get("/my_trivia", (req, res) => {
-  res.render("./html/my_trivia");
+  // let initialAnswers = [];
+  let initialAnswers = 0;
+  let score = 0;
+  res.render('./html/my_trivia',{userAnswers: initialAnswers});
 });
+// let userAnswers = [];
+// let userAnswerList = [];
+app.post('/my_trivia', (req,res) => {
+  // userAnswerList.push(req.body.userAnswer);
+  res.render('.html/my_trivia',{userAnswers: req.body.userAnswer})
+  // res.render('./html/my_trivia', {userAnswers: userAnswerList});
+  // res.send(`Your Answer: ${userAnswer}`);
+  // if (userAnswer.toUpperCase() == "D") {
+  //   res.send(`Your Answer: ${userAnswer}, Correct`);
+    // res.send('Correct');  
+  // }
+  // else {
 
-app.post("/my_trivia", (req,res) => {
-  let userAnswer = req.body.userAnswer1
-  if (userAnswer == "D") {
-    res.send(`Correct`);  
-  }
-  else {
-    res.send(`Incorrect`);
-  }
+  //   res.render("./html/my_fun_facts");
+  // }
 });
 
 // Setup server ports
---
+++ public/js/my_trivia.js
@@ -0,0 +1,9 @@
+// const submittedAnswer = document.getElementById('submitAnswer');
+
+// submitAnswer.addEventListener('click',(e) => {
+//     let userAnswer = document.getElementById('userAnswer');
+//     console.log(userAnswer.value);
+//     submitAnswer.parentElement(userAnswer.value)
+// });
+
+
